8d621f44af36c62aaecd2cfe7246c8c1cf9ad257
wanghaifei <whf.java@163.com>
2014-09-01 13:36:37 -0700

    dd

--- application/controllers/crontab.php
+++ application/controllers/crontab.php
@@ -257,7 +257,8 @@ class Crontab extends CI_Controller {
                 }
                 $id = $detail_info['_id'];
                 unset($detail_info['_id']);
-                print_r($detail_info);exit;
+                print_r($detail_info);
+                exit;
                 $this->detail_model->update($id, $detail_info);
             }
         }
