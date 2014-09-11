f9512a336108cd8de419d7b47cc49293a6c99b29
wanghaifei <whf.java@163.com>
2014-09-10 18:55:42 -0700

    dd

--- application/controllers/crontab.php
+++ application/controllers/crontab.php
@@ -209,6 +209,7 @@ class Crontab extends CI_Controller {
                 if($status === 0){
                     $crawl_info['url'] = $url;
                     $this->queue_model->add_queue(self::Q_RELATION, $crawl_info);
+                    $this->relation_model->update_lasttime($crawl_info['_id']);
                 }
             }
         }
