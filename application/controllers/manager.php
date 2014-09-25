03bdcc26df79473c61ec559aa2dcf785aa973260
wanghaifei <whf.java@163.com>
2014-09-24 17:47:10 -0700

    dd

--- application/controllers/manager.php
+++ application/controllers/manager.php
@@ -32,9 +32,7 @@ class Manager extends CI_Controller {
     public function find_class()
     {
         $id = '53c8f7f1eb8be2e6498b4567';
-
-        $info = array('_id'=>new MongoId("$id"));
-        $result = $this->class_model->findOne($info);
+        $result = $this->class_model->findOneByID($id);
         print_r($result);
     }
 
